864177100b130c1bb13789ca4cc6eb17979fde2b
Ivan <mulleivan@gmail.com>
2013-07-05 16:21:20 +1200

    styles

--- app/controllers/demographic_controller.rb
+++ app/controllers/demographic_controller.rb
@@ -7,6 +7,5 @@ class DemographicController < ApplicationController
   def get
     strong_params =params.require(:demographic).permit(:payer_name, :payer_id, :provider_last_name, :provider_first_name, :provider_npi, :member_id, :member_last_name, :member_first_name, :member_dob, :member_gender, :internal_id)
     @demographic = Eligible::Demographic.get(strong_params)
-
   end
 end
